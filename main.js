5e4b80fa70cb4b8c65b7fdf9a200d9d88fbb3b17
Ananmay 125 <ananmay125@gmail.com>
2024-02-25 01:28:59 -0800

    First Commit
    Bruh...

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const body = document.body;
+    const images = ["images/sanic-.jpg","images/huh_cat.jpg","images/nahh.jpg"];
+
+    let index = 0;
+
+    function changeBackground() {
+        body.style.backgroundImage = `url(${images[index]})`;
+        index = (index + 1) % images.length;
+    }
+
+    setInterval(changeBackground, 10000);
+    changeBackground();
+});
