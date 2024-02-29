614baf9077211b69dfad535814fe7866521e6fbb
Ananmay 125 <ananmay125@gmail.com>
2024-02-29 19:33:19 +0530

    fixing the hero-section

--- main.js
+++ main.js
@@ -4,20 +4,16 @@ var currentImageIndex = 0;
 function changeImage() {
     var bruhImage = document.getElementById("bruh-image");
 
-    // Set opacity to 0 to create a fade-out effect
     bruhImage.style.opacity = 0;
 
-    // Change the image source after a short delay
     setTimeout(function() {
         bruhImage.src = imageArray[currentImageIndex];
-    }, 500); // Use a shorter delay than the transition duration
+    }, 500);
 
-    // Set opacity back to 1 to create a fade-in effect after the transition duration
     setTimeout(function() {
         bruhImage.style.opacity = 1;
-    }, 500); // Use the same duration as the transition (1 second)
+    }, 500);
     
-    // Increment the index for the next image
     currentImageIndex = (currentImageIndex + 1) % imageArray.length;
 }
 
